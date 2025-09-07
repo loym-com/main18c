0af5b93968695ce5da0136e0a0a98aaa22253082
Henrik Norlin <henrik.norl@gmail.com>
2025-09-07 17:06:40 +0200

    main18c ready for test

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,10 @@
 {
     "name": "Dependencies",
     "depends": [
+        # MANDATORY
+        "auth_admin_passkey",
+        "dbfilter_from_header",
+        # OPTIONAL
         "web_responsive",
     ],
     "version": "18.0.1.0.0",
